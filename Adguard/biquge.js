4e784cda7dc3d2c5d820ecb48d32e4a7072f181f
limbopro <service.limbopro.xyz@gmail.com>
2021-04-28 09:40:19 +0800

    hp echo-response new and more

---
+++ Adguard/biquge.js
@@ -0,0 +1,1 @@
+document.cookie = "hitme=1";
--- Adguard/hpjav.js
+++ Adguard/hpjav.js
@@ -1,2 +1,15 @@
+/*
 var hpjav = document.querySelectorAll("div.container");
 hpjav[2].remove();
+*/
+
+var hpjavbox = document.querySelectorAll("div.home-img-box");
+hpjavbox[1].style.display = "none";
+
+/*
+var target = document.querySelectorAll("[target*=_blank]");
+var i;
+for (i = 0; i < target.length; i++) {
+target[i].style.display = "none";
+}
+*/

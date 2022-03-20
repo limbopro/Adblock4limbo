c153a231332f0a2750c3ede0be2b0af896fcbe93
limbopro <service.limbopro.xyz@gmail.com>
2022-03-20 15:59:35 +0800

    在 jableX.js 文件中新增了document.cookie = "ts_popunder=1"; 以解决每日首次打开 https://jable.tv/ 会有硬弹窗的问题。

--- Adguard/jable.js
+++ Adguard/jable.js
@@ -1,3 +1,5 @@
 var Jable = document.querySelectorAll("div.col-6.col-sm-4.col-lg-12");
 Jable[0].style.display = "none";
 document.cookie = "zone-cap-3403709=1";
+document.cookie = "kt_tcookie=1";
+document.cookie = "ts_popunder=1";
--- Adguard/jableX.js
+++ Adguard/jableX.js
@@ -1,1 +1,3 @@
 document.cookie = "zone-cap-3403709=1";
+document.cookie = "kt_tcookie=1";
+document.cookie = "ts_popunder=1";

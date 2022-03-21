c9ffa6bfcc26604a2a2b30d5881d1b5c246e2fc6
limbopro <service.limbopro.xyz@gmail.com>
2022-03-21 11:37:53 +0800

    Something Fixed.

--- Adguard/contentFarm.js
+++ Adguard/contentFarm.js
@@ -3,7 +3,7 @@ Written by limbopro
 https://limbopro.com/archives/block-contentfarm-Quantumultx.html
 https://t.me/Adblock4limbo
 There are 4618 content farm domains in total until now.
-Last updated at 19/3月/2022/23:40
+Last updated at 20/3月/2022/23:40
 */
 
 
--- Adguard/jable.js
+++ Adguard/jable.js
@@ -3,3 +3,27 @@ Jable[0].style.display = "none";
 document.cookie = "zone-cap-3403709=1";
 document.cookie = "kt_tcookie=1";
 document.cookie = "ts_popunder=1";
+
+var webList = [
+'r.trwl1.com', 
+'kknews.cc',
+'021shfx.com',
+'024ksm.com',
+'025pc.cn'
+];
+
+
+for (l = 0; l < webList.length; l++) {
+var webSelectors = "a[href*='" + webList[l] + "']";
+
+var google_cssSelectors = [".video-img-box.mb-e-20,.col-6.col-sm-4.col-lg-3"];
+var huge = document.querySelectorAll( google_cssSelectors );
+var i;
+
+for (i=0; i< huge.length; i++){
+if (huge[i].querySelectorAll( webSelectors ).length  > 0) {
+huge[i].style.display = "none";
+}
+}
+
+}

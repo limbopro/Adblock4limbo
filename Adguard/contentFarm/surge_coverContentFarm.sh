91250e44e421b09fe2ea63ee1a508b45e98e667b
limbopro <service.limbopro.xyz@gmail.com>
2022-03-31 13:47:40 +0800

    remove contentfarm

--- Adguard/contentFarm/surge_coverContentFarm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/typecho/
-rm surge_Adblock4limbo.list
-cp Adblock4limbo.list surge_Adblock4limbo.list
-sed -i s/reject/''/g `grep reject -rl --include="surge_Adblock4limbo.list" ./*` 
-sed 's/.$//' surge_Adblock4limbo.list >> surge_Adblock4limbo.list.x
-sed 's/.$//' surge_Adblock4limbo.list.x >> surge_Adblock4limbo.list

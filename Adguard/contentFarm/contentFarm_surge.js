7a2eca00003a5793e70be4fab754180afd8ba325
limbopro <service.limbopro.xyz@gmail.com>
2022-03-31 13:46:14 +0800

    contentfarm fixed.

--- Adguard/contentFarm/contentFarm_surge.js
+++
@@ -1,5 +0,0 @@
-let Old = '</body>'
-let New = '<link rel="stylesheet" href="https://limbopro.com/CSS/contentFarm.css"><script  src="https://limbopro.com/Adguard/contentFarm/contentFarm.js"></script></body>';
-let body = $response.body
-.replace(Old, New);
-$done({body});

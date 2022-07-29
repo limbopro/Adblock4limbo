58d87dcaf85915f02e4e5022cca7a9d7f2c45b3f
limbopro <service.limbopro.xyz@gmail.com>
2022-07-30 00:51:09 +0800

    TG u r bad bad.

--- Adguard/contentFarm/contentFarm.js
+++ Adguard/contentFarm/contentFarm.js
@@ -3,7 +3,7 @@ Written by limbopro
 https://limbopro.com/archives/block-contentfarm.html
 https://t.me/Adblock4limbo
 There are 4618 content farm domains in total until now.
-Last updated at 21/7月/2022/23:41
+Last updated at 30/7月/2022/00:50
 */
 
 

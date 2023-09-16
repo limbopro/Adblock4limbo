78c08252ee9d8f7e8e226216603b077519ad6fc5
limbopro <service.limbopro.xyz@gmail.com>
2023-09-17 00:40:59 +0800

    https://netflav.com/ 修复（支持 Surge/QuantumultX/Shadowrocket/油猴浏览器扩展用户）

--- Adguard/Adblock4limbo.user.js
+++ Adguard/Adblock4limbo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Adblock4limbo.X
 // @namespace    https://github.com/limbopro/Adblock4limbo/raw/main/Adguard/Adblock4limbo.user.js
-// @version      0.3.6.4
+// @version      0.3.6.5
 // @license      CC BY-NC-SA 4.0
 // @description  毒奶去广告计划油猴版；通过 JavaScript 移除Pornhub/搜索引擎（Bing/Google）广告及内容农场结果清除/泥巴影视/低端影视（可避免PC端10秒广告倒计时）/独播库/ibvio/Jable（包含M3U8文件提取）/MissAv（禁止离开激活窗口视频自动暂停播放）/禁漫天堂/紳士漫畫/91porn/哔滴影视（加速跳过视频广告/避免反查）/555电影网（o8tv）等视频网站上的视频广告和图片广告，保持界面清爽干净无打扰！其他：优化PC端未登录状态访问知乎浏览体验（动态移除登录窗口/永远不会跳转至首页登录页面）；
 // @author       limbopro
@@ -65,6 +65,8 @@
 // @match        https://4hu.tv/*
 // @match        https://netflav.com/*
 // @match        https://filemoon.sx/*
+// @match        https://embedrise.com*
+// @match        https://mmfl02.com/*
 // @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=limbopro.com
 // @require https://greasyfork.org/scripts/442253-%E5%B1%8F%E8%94%BD%E5%86%85%E5%AE%B9%E5%86%9C%E5%9C%BA-with-%E6%B2%B9%E7%8C%B4%E8%84%9A%E6%9C%AC/code/%E5%B1%8F%E8%94%BD%E5%86%85%E5%AE%B9%E5%86%9C%E5%9C%BA%EF%BC%88with%20%E6%B2%B9%E7%8C%B4%E8%84%9A%E6%9C%AC%EF%BC%89.user.js
@@ -224,7 +226,9 @@ function values() {
         "javbus",
         "4hu",
         "netflav",
-        "filemoon.",
+        "filemoon",
+        "embedrise",
+        "mmfl02",
         "zhihu"
     ]
 
@@ -443,11 +447,18 @@ function adsDomain_switch(x) { // 匹配参数值 执行相应函数
             hrefAttribute_set();
             break;
         case "netflav":
-            //addEventListener_defuser("touchend");
+            window_open_defuser(); // 打断 window.open 施法
             css_adsRemove(imax.css.netflav, 0, "4hu");
             break;
         case "filemoon":
+            window_open_defuser(); // 打断 window.open 施法
             //nowindowopenif();
+            break;
+        case "embedrise":
+            window_open_defuser(); // 打断 window.open 施法
+            break;
+        case "mmfl02":
+            window_open_defuser(); // 打断 window.open 施法
             break;
         case 'zhihu':
             var zhihu_id = "zhihux"

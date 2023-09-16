6a7b47d717c5cb03fb94e0ff0db11b1eebbc1efc
limbopro <service.limbopro.xyz@gmail.com>
2023-09-16 16:50:53 +0800

    修复 Adblock4limbo.list / Adblock4limbo_surge.list 格式

--- Adguard/contentFarm/contentFarm.user.js
+++ Adguard/contentFarm/contentFarm.user.js
@@ -3,7 +3,7 @@ Written by limbopro
 https://limbopro.com/archives/block-contentfarm.html
 https://t.me/Adblock4limbo
 There are 1798 content farm domains in total until now.
-Last updated at 16/9月/2023/10:06
+Last updated at 16/9月/2023/16:50
 */
 
 

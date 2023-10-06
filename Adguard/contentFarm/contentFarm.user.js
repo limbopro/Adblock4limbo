aa49b0d084b7de68167d2dedb627495c3e9a616d
limbopro <service.limbopro.xyz@gmail.com>
2023-10-07 00:00:10 +0800

    Automatic updates

--- Adguard/contentFarm/contentFarm.user.js
+++ Adguard/contentFarm/contentFarm.user.js
@@ -3,7 +3,7 @@ Written by limbopro
 https://limbopro.com/archives/block-contentfarm.html
 https://t.me/Adblock4limbo
 There are 7165 content farm domains in total until now.
-Last updated at  6/10月/2023/00:00
+Last updated at  7/10月/2023/00:00
 */
 
 

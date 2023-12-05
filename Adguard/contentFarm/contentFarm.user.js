8340baba4143caa6edc68e911bebf31bb75f1513
limbopro <service.limbopro.xyz@gmail.com>
2023-12-05 19:48:03 +0800

    新增去广告分流规则以移除 missav.com 页面广告

--- Adguard/contentFarm/contentFarm.user.js
+++ Adguard/contentFarm/contentFarm.user.js
@@ -3,7 +3,7 @@ Written by limbopro
 https://limbopro.com/archives/block-contentfarm.html
 https://t.me/Adblock4limbo
 There are 7166 content farm domains in total until now.
-Last updated at  5/12月/2023/00:00
+Last updated at  5/12月/2023/19:46
 */
 
 

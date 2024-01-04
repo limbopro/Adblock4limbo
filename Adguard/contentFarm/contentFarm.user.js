9c940168a24539ee91ac7d091cb50ace9c633fec
limbopro <service.limbopro.xyz@gmail.com>
2024-01-05 00:00:09 +0800

    系统自动更新 Automatic updates // 一般为更新分流规则以及内容农场域名清单

--- Adguard/contentFarm/contentFarm.user.js
+++ Adguard/contentFarm/contentFarm.user.js
@@ -3,7 +3,7 @@ Written by limbopro
 https://limbopro.com/archives/block-contentfarm.html
 https://t.me/Adblock4limbo
 There are 7166 content farm domains in total until now.
-Last updated at  4/1月/2024/00:00
+Last updated at  5/1月/2024/00:00
 */
 
 

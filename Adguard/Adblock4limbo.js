797bf3f4530fb96c68387dd73dd23223a7c8c57d
limbopro <service.limbopro.xyz@gmail.com>
2024-11-01 21:25:29 +0800

    修正 Adblock4limbo.js 中判断响应体是否为 null  if 条件判断

--- Adguard/Adblock4limbo.js
+++ Adguard/Adblock4limbo.js
@@ -1,4 +1,5 @@
 /**
+ * 
  * ---------------------------
  * 毒奶去网页广告计划
  * Author: limbopro
@@ -8,6 +9,7 @@
  * FAQ：https://t.me/Adblock4limbo/21 常见问题与回答
  * Github：https://github.com/limbopro/Adblock4limbo
  * ---------------------------
+ * 
  */
 
 /*
@@ -29,7 +31,10 @@ var modifiedHeaders = $request.headers;
 modifiedHeaders['User-Agent'] = 'Mozilla/6.0 (iPhone 15; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/116.0.5845.118 Mobile/15E148 Safari/604.1';
 */
 
+
 // 定义 全局CSS/JS 用于移除网页上的广告元素、禁止点击弹窗广告等
+
+
 var regex = '</title>';
 var replace_str = '</title>\
 <link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
@@ -40,7 +45,7 @@ let url = $request.url;
 var url_target_regex = /(missav|netflav|supjav|njav|javday)/g;
 
 var url_target = url.match(url_target_regex);
-if ($response.body) {  // 判断响应体是否存在
+if ($response.body !== null) {  // 判断响应体是否存在
     if (url_target) {  // 判断该URL是否匹配目标
         let window_open_reg = 'window.open'; // 匹配
         let window_open_str = ''; // 替换为空
@@ -53,3 +58,4 @@ if ($response.body) {  // 判断响应体是否存在
     headers['Content-Security-Policy'] = '*';
     $done({ headers: headers, body: body, url: url })
 }
+
--- Adguard/Adblock4limbo.user.js
+++ Adguard/Adblock4limbo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Adblock4limbo.[github]
 // @namespace    https://github.com/limbopro/Adblock4limbo/raw/main/Adguard/Adblock4limbo.user.js
-// @version      0.2024.10.30
+// @version      0.2024.11.01
 // @license      CC BY-NC-SA 4.0
 // @description  毒奶去网页广告计划用户脚本 For Quantumult X & Surge & Shadowrocket & Loon & Stash & 油猴 ；1.新增页面右下角导航；2.通过 JavaScript 移除特定网站网页广告 —— 搜索引擎（Bing/Google）广告及内容农场结果清除/低端影视/欧乐影院/iyf爱壹帆/哔滴影视/Pornhub/Javbus/Supjav/Jable/MissAv/91porn/hitomi/紳士漫畫/禁漫天堂/等视频&ACG&小说&漫画网站上的弹窗广告&视频广告&Gif图片广告等，保持网页清爽干净无打扰！ P.S. 欢迎提交issue
 // @author       limbopro

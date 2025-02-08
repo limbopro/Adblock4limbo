341473b8560246046018dbb34c1de8b655a7b647
limbopro <service.limbopro.xyz@gmail.com>
2025-02-08 21:57:12 +0800

    暂时移除 missav.ws 页面中有关 石川澪 的AD banner

--- Adguard/Adblock4limbo.user.js
+++ Adguard/Adblock4limbo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Adblock4limbo.[github]
 // @namespace    https://github.com/limbopro/Adblock4limbo/raw/main/Adguard/Adblock4limbo.user.js
-// @version      0.2025.02.05
+// @version      0.2025.02.07
 // @license      CC BY-NC-SA 4.0
 // @description  毒奶去网页广告计划用户脚本 For Quantumult X & Surge & Shadowrocket & Loon & Stash & 油猴 ；1.新增页面右下角导航；2.通过 JavaScript 移除特定网站网页广告 —— 搜索引擎（Bing/Google）广告及内容农场结果清除/低端影视/欧乐影院/iyf爱壹帆/哔滴影视/Pornhub/Javbus/Supjav/Jable(支持抓取M3U8链接)/MissAv/91porn(支持视频下载)/hitomi/紳士漫畫/禁漫天堂/等视频&ACG&小说&漫画网站上的弹窗广告&视频广告&Gif图片广告等，保持网页清爽干净无打扰！ P.S. 欢迎提交issue
 // @author       limbopro
@@ -1420,6 +1420,14 @@ function adsDomain_switch(x) { // 匹配参数值 执行相应函数
                     addListenerById("missavPause", () => { video_pause() }, 1000);
                 }
             }, 3000)
+
+            document.querySelectorAll('div.grid').forEach(
+                (x) => {
+                    if (x.querySelector('img[src*="mio.jpg"]')) { // 移除 missav 播放页广告
+                        x.style = 'display:none !important;'
+                    }
+                }
+            )
 
             break;
         default:
